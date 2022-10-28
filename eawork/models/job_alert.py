ef98be80dfd74215c529330b883d6f46079e3c3b
Conor Barnes <conorbarnes93@gmail.com>
2022-10-28 10:23:03 -0300

    use a virtual method for query_string

--- eawork/models/job_alert.py
+++ eawork/models/job_alert.py
@@ -1,5 +1,6 @@
 from secrets import token_hex
-
+from django.utils.http import urlencode
+import urllib.parse
 from auditlog.models import AuditlogHistoryField
 from auditlog.registry import auditlog
 from django.db import models
@@ -12,7 +13,7 @@ from eawork.models.time_stamped import TimeStampedModel
 class JobAlert(TimeStampedModel):
     email = models.EmailField(max_length=511)
     query_json = JSONField(null=True, blank=True)
-    query_string = models.CharField(max_length=4095, blank=True)
+    query_string = models.CharField(max_length=4095, blank=True) # legacy, to be deleted
     last_checked_at = models.DateTimeField(null=True, blank=True, default=timezone.now)
     post_pk_seen_last = models.PositiveIntegerField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
@@ -22,6 +23,37 @@ class JobAlert(TimeStampedModel):
 
     def __str__(self):
         return self.email
+
+    def generate_query_string(self) -> str:
+        query_string = "?"
+
+        if self.query_json is None:
+            return query_string
+
+        # keyword
+        keyword_query = self.query_json["query"]
+        if keyword_query:
+            query_string = query_string + "query=" + keyword_query
+
+        # facets
+        for subarr in self.query_json["facetFilters"]:
+            for i, key_val in enumerate(subarr):
+                [key, val] = key_val.split(":")
+
+                if (query_string != "?"):
+                    query_string = query_string + "&"
+
+                query_string = (
+                    query_string
+                    + "refinementList%5B"
+                    + urllib.parse.quote(key)
+                    + "%5D%5B"
+                    + str(i)
+                    + "%5D="
+                    + urllib.parse.quote(val)
+                )
+
+        return query_string
 
 
 auditlog.register(JobAlert)
